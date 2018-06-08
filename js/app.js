e73152cc5950d6f9c72b2eb8618c3eb43ffcd952
Developer <developer@IM-M11-877.local>
2018-06-08 17:10:19 -0500

    avance

--- js/app.js
+++ js/app.js
@@ -35,5 +35,34 @@ $(document).ready(function(){
     }
   });
 
+  $("#txt").focusin(function(){
+    $(this).addClass("myClass");
+  });
+  $("#txt").focusout(function(){
+    if($(this).val().length >= 10 && $(this).val().length <= 20){
+      $("#id5").html("<span style = 'color: #2ECC71'>Válido</span>")
+    }
+    else if ($(this).val().length < 10 ) {
+      $("#id5").html("<span style = 'color: #CD6155'>Escribe min 10 caracteres</span>")
+    }
+
+  });
+
+  $("#email").focusin(function(){
+    $(this).addClass("myClass");
+  });
+  $("#email").focusout(function(){
+    mailno = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if (mailno.test($(this).val())) {
+      $("#id4").html("<span style = 'color: #2ECC71'>Válido</span>")
+    } else {
+      $("#id4").html("<span style = 'color: #CD6155'>mail invalido</span>")
+    }
+  });
+
+$("#btn").click(function(){
+  alert("Se ha enviado el formulario.")
+});
+
 
 });
